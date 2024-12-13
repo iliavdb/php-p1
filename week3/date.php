db4c29397b737d676f4f2359a81af475464ecad7
iliavdb <9026087@student.zadkine.nl>
2024-12-13 13:46:25 +0100

    week 3

---
+++ week3/date.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "vandaag is het: " . date("2024-August-19")
+    echo "deze dag is het dag van het jaar : " . ("231")
+    echo "maandag is the eerste dag van het week : " . ("1ste")
+    echo "Deze maand heeft : " . ("31")
+</body>
+</html>
---
+++ week3/randNmr.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><center>Opdrachten week 3</center></h1>
+    <h2><center>Opdracht 1</center></h2>
+    <h3><center>Opdracht 2</center></h3>
+    
+    
+</body>
+</html>
